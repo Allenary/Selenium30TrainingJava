52f8d2f6dc239fabfdde1ef46580a00de71a89d6
Allenary <kochetovanina@gmail.com>
2017-01-03 17:25:17 +0200

    added functionality: fill-in data on Information and Prices pages added TODOs

--- src/test/java/litecartAdmin/AddProductTest.java
+++ src/test/java/litecartAdmin/AddProductTest.java
@@ -8,6 +8,7 @@ import java.util.Date;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import testHelper.DateHelper;
 import testHelper.TestAncestor;
@@ -17,6 +18,13 @@ public class AddProductTest extends TestAncestor {
 
 	@Test
 	public void test() {
+		/*TODO: verify in FF
+		 *  change names to unique
+		 *   change selectors to most efficient than searching by text
+		 *   add assertion
+		 *   make product enabled
+		 *   use pageObject
+		 */
 		(new AdminLoginPage(driver)).login();
         driver.get("http://litecart.resscode.org.ua/admin/?app=catalog&doc=catalog");
         driver.findElement(By.linkText("Add New Product")).click();
@@ -26,8 +34,9 @@ public class AddProductTest extends TestAncestor {
         WebElement quantity =driver.findElement(By.name("quantity")); 
         quantity.clear();
         quantity.sendKeys("3");
-        File file = new File("resources/test.jpg");
+        File file = new File("src/test/resources/test.jpg");
         String absolutePath = file.getAbsolutePath();
+        System.out.println(absolutePath);
         driver.findElement(By.name("new_images[]")).sendKeys(absolutePath);
         
         Date dateStart = new Date();
@@ -36,7 +45,22 @@ public class AddProductTest extends TestAncestor {
         Date dateEnd = DateHelper.getTodayPlusOneYear();
         driver.findElement(By.name("date_valid_to")).sendKeys(dateFormat.format(dateEnd));
         
-  
+        //Information tab
+        driver.findElement(By.linkText("Information")).click();
+        new Select(driver.findElement(By.name("manufacturer_id"))).selectByIndex(1);
+        driver.findElement(By.name("keywords")).sendKeys("test keyword");
+        driver.findElement(By.name("short_description[en]")).sendKeys("test short desription");
+        
+        driver.findElement(By.className("trumbowyg-editor")).sendKeys("test full description");
+        driver.findElement(By.name("head_title[en]")).sendKeys("test title");
+        driver.findElement(By.name("meta_description[en]")).sendKeys("test meta");
+        
+        //Prices tab
+        driver.findElement(By.linkText("Prices")).click();
+        driver.findElement(By.name("purchase_price")).sendKeys("5");
+        new Select(driver.findElement(By.name("purchase_price_currency_code"))).selectByValue("USD");
+        
+       driver.findElement(By.name("save")).click();
 	}
 
 }
