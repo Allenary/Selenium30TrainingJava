c773c4ba8ba6d082fe581c479830404a335d28a7
Nina D <ndovhobrod@gmail.com>
2017-01-04 14:44:01 +0200

    make separate class for generating unique string

--- src/test/java/litecartShop/UserRegistration.java
+++ src/test/java/litecartShop/UserRegistration.java
@@ -2,6 +2,7 @@ package litecartShop;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import static testHelper.DataGenerator.getUniqueString;
 
 import testHelper.TestAncestor;
 
@@ -9,27 +10,30 @@ public class UserRegistration extends TestAncestor{
 
 	@Test
 	public void test() {
+            String email = "mail"+getUniqueString()+"@test.com";
+            String password = "testPass123";
+            
 		driver.get("http://litecart.resscode.org.ua/en/create_account");
 		driver.findElement(By.name("firstname")).sendKeys("testFirstName");
 		driver.findElement(By.name("lastname")).sendKeys("testLastName");
 		driver.findElement(By.name("address1")).sendKeys("testAddress1");
 		driver.findElement(By.name("postcode")).sendKeys("12345");
 		driver.findElement(By.name("city")).sendKeys("testCity");
-		String email = "mail"+System.currentTimeMillis()+"@test.com";
 		
 		driver.findElement(By.name("email")).sendKeys(email);
 		driver.findElement(By.name("phone")).sendKeys("123456789");
-		String password = "testPass123"; 
+		 
 		driver.findElement(By.name("password")).sendKeys(password);
 		driver.findElement(By.name("confirmed_password")).sendKeys(password);
 		driver.findElement(By.name("create_account")).click();
 		
+//logout
 		driver.findElement(By.cssSelector("#box-account li:last-child a")).click();
 		
 		driver.findElement(By.name("email")).sendKeys(email);
 		driver.findElement(By.name("password")).sendKeys(password);
 		driver.findElement(By.name("login")).click();
-		
+//logout		
 		driver.findElement(By.cssSelector("#box-account li:last-child a")).click();
 	}
 
---
+++ src/test/java/testHelper/DataGenerator.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testHelper;
+
+/**
+ *
+ * @author nd
+ */
+public class DataGenerator {
+    
+    public static String getUniqueString(){
+        return String.valueOf(System.currentTimeMillis());
+    }
+    
+}
